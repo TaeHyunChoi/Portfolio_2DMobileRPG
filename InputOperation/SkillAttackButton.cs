0a89b4d7be98bae4ad24b1dab41bc015fb41580d
TaeHyunChoi <83195176+TaeHyunChoi@users.noreply.github.com>
2022-01-04 18:10:20 +0900

    Add files via upload

---
+++ InputOperation/SkillAttackButton.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SkillAttackButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    InputManager _inputMNG;
+
+    [SerializeField] string _skillPawn;
+    private List<Pawn> _ltPlayerParty;
+    private Image _imgSKILL;
+    public string SkillPawn { get { return _skillPawn; } set { _skillPawn = value; } }
+    private int _pawnIndex;
+
+    private void Start()
+    {
+        _inputMNG = GameObject.FindGameObjectWithTag("InputManager").GetComponent<InputManager>();
+        _imgSKILL = transform.GetComponent<Image>();
+        _imgSKILL.color = _inputMNG._colorDefault;
+        _ltPlayerParty = IngameManager._instance._ltPartyPawns;
+    }
+
+    //[초기화] 각 스킬 버튼에 플레이어, 동료AI 인덱스 번호를 입력
+    public void InitButtonInfos(int pawnIndex)
+    {
+        _pawnIndex = pawnIndex;
+    }
+
+    //스킬 사용이 가능할 때에 스킬 버튼을 누르면 → 해당 캐릭터에게 스킬 입력을 호출
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _imgSKILL.color = _inputMNG._colorPressed;
+
+        if (_ltPlayerParty[_pawnIndex]._skill.PossibleUse)
+        {
+            if (_ltPlayerParty[_pawnIndex].PawnType == PublicDefines.ePawnType.Player)
+            {
+                _ltPlayerParty[_pawnIndex].InputState = PublicDefines.NowAction.SKILL;
+            }
+            else
+            {
+                _ltPlayerParty[_pawnIndex].GetComponent<PawnFellower>().Input_External(PublicDefines.NowAction.SKILL);
+            }
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _imgSKILL.color = _inputMNG._colorDefault;
+    }
+}
---
+++ InputOperation/VirtualPadControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class VirtualPadControl : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler, IPointerExitHandler
+{
+    [SerializeField] PublicDefines.InputDirection _dir;
+    InputManager _inputMNG;
+    RectTransform _rootPad;
+
+    Vector3 _inputVector;
+    bool _nowInput;
+
+    private void Start()
+    {
+        _rootPad = transform.parent.GetComponent<RectTransform>();
+        _inputMNG = GameObject.FindGameObjectWithTag("InputManager").GetComponent<InputManager>();
+    }
+    private void Update()
+    {
+        if (_nowInput)
+        {
+            _inputMNG.SetInputVector(_inputVector);     //이동 입력값을 받으면
+            _inputMNG.ActivateMoveButton();             //값에 맞춰 버튼 활성화
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        _nowInput = true;
+        Vector2 pos;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(_rootPad, eventData.position, eventData.pressEventCamera, out pos))
+        {
+            pos.x = (pos.x / _rootPad.sizeDelta.x);
+            pos.y = (pos.y / _rootPad.sizeDelta.y);
+
+            _inputVector = new Vector3(pos.x, pos.y, 0);
+            _inputVector = (_inputVector.magnitude > 1) ? _inputVector.normalized : _inputVector;
+        }
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        _nowInput = false;
+        _inputVector = Vector3.zero;
+        _inputMNG.SetInputVector(_inputVector);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        OnPointerUp(eventData);
+    }
+}
